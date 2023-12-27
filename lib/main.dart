9a7f5c656921b273e3998ef1f2efaf88499602db
Sabin Regmi <get2sabin@gmail.com>
2023-12-27 13:11:30 -0500

    Added Riverpod Dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:modbus_simulator/src/rust/api/simple.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:modbus_simulator/src/rust/frb_generated.dart';
 
 Future<void> main() async {
   await RustLib.init();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ipTextEditingController = useTextEditingController();
+    final portTextEditingController = useTextEditingController();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -19,6 +24,7 @@ class MyApp extends StatelessWidget {
         children: <Widget>[
           const HeaderComponent(),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               const Text(
                 'Protocol: TCP',
@@ -27,30 +33,32 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.w400,
                 ),
               ),
-              const Expanded(
+              Expanded(
                 child: Padding(
-                  padding: EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: TextField(
-                    decoration: InputDecoration(
+                    controller: ipTextEditingController,
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'IP Address',
                     ),
                   ),
                 ),
               ),
-              const Expanded(
+              Expanded(
                 child: Padding(
-                  padding: EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: TextField(
-                    decoration: InputDecoration(
+                    controller: portTextEditingController,
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Port',
                     ),
                   ),
                 ),
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.stop))
+              IconButton(onPressed: () {}, icon: const Icon(Icons.play_arrow)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.stop))
             ],
           )
         ],
